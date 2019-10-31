a15b3876230a64be76b941d485a18e073b57284d
Owen Smith <ogs22@cam.ac.uk>
2019-10-31 12:28:39 +0000

    webbify it

--- sd.go
+++ sd.go
@@ -3,17 +3,18 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/fatih/color"
 	"github.com/sergi/go-diff/diffmatchpatch"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/sheets/v4"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"regexp"
+	"strings"
 )
 
 type MySheet struct {
@@ -64,63 +65,100 @@ type MySheet struct {
 	SpreadsheetURL string `json:"spreadsheetUrl"`
 }
 
+type Results struct {
+	Title	string
+	Gs_old	string
+	Gs_new string
+	Info []string
+	Output template.HTML
+}
+
 func main() {
+	http.HandleFunc("/", xmain)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func xmain(w http.ResponseWriter, r *http.Request) {
 	var gs_old_props, gs_new_props MySheet
+	var error string
 
-	info := color.New(color.FgCyan).Add(color.Underline)
-
-	if len(os.Args) < 2 {
-		log.Printf("Usage %s SpreadSheetID_old SpreadSheetID_new", os.Args[0])
-		log.Fatalf("Incorrect command line args")
+	urlParts := strings.Split(r.URL.Path,"/")
+	fmt.Printf("len=%d cap=%d %v\n", len(urlParts), cap(urlParts), urlParts)
+	if len(urlParts) < 3 {
+		fmt.Fprint(w, http.StatusNotFound);
+		log.Printf("URL not valid %s", urlParts)
+		return
 	}
 
-	gs_old := os.Args[1]
-	gs_new := os.Args[2]
+	gs_old := urlParts[1]
+	gs_new := urlParts[2]
+
+	data := Results{
+		Title:  "Comparsion of Google Sheets",
+		Gs_old: gs_old,
+		Gs_new: gs_new,
+		Output: "None",
+	}
+
 
 	var validID = regexp.MustCompile(`[a-zA-Z0-9-_]+`)
 
 	if validID.MatchString(gs_old) && validID.MatchString(gs_new) {
 		// ids are valid
-		info.Printf("\nComparing sheet %s and %s\n", gs_old, gs_new)
+		data.Info = append(data.Info , fmt.Sprintf("\nComparing sheet %s and %s\n", gs_old, gs_new))
+		log.Printf("\nComparing sheet %s and %s\n", gs_old, gs_new)
 	} else {
-		log.Fatalf("Input IDs not valid \n%s \n%s", gs_old ,gs_new)
+		error = fmt.Sprintf("Input IDs not valid \n%s \n%s", gs_old ,gs_new)
+		errorToScreen(error, w)
+		return
 	}
-
-
-
 	//get *sheets.Service from quickstart
 	service := qsmain()
 	// get the props for the old sheets
 	sother_old, err := service.Spreadsheets.Get(gs_old).Do()
 	if err != nil {
-		log.Fatalf("\nUnable to retrieve data from spreadsheet\nID:%s: %v",gs_old, err)
+		error = fmt.Sprintf("\nUnable to retrieve data from spreadsheet\nID:%s: %v",gs_old, err)
+		errorToScreen(error, w)
+		return
 	}
 	tmp_old, err := sother_old.MarshalJSON()
 	err = json.Unmarshal(tmp_old, &gs_old_props)
 	if err != nil {
-		log.Fatalf("\nUnable to Unmarshall from spreadsheet\nID:%s: %v",gs_old, err)
+		error = fmt.Sprintf("\nUnable to Unmarshall from spreadsheet\nID:%s: %v",gs_old, err)
+		errorToScreen(error, w)
+		return
 	}
 	sother_new, err := service.Spreadsheets.Get(gs_new).Do()
 	if err != nil {
-		log.Fatalf("\nUnable to retrieve data from spreadsheet\nID:%s: %v",gs_new, err)
+		error = fmt.Sprintf("\nUnable to retrieve data from spreadsheet\nID:%s: %v",gs_new, err)
+		errorToScreen(error, w)
+		return
 	}
 	tmp_new, err := sother_new.MarshalJSON()
 	err = json.Unmarshal(tmp_new, &gs_new_props)
 	if err != nil {
-		log.Fatalf("\nUnable to Unmarshall from spreadsheet\nID:%s: %v",gs_new, err)
+		error = fmt.Sprintf("\nUnable to Unmarshall from spreadsheet\nID:%s: %v",gs_new, err)
+		errorToScreen(error, w)
+		return
 	}
-	info.Println("\nChecking Spreadsheets have the same number of sheets and sheet names")
-	_ = cmpSheetTitles(gs_old_props, gs_new_props)
-
+	data.Info = append(data.Info , fmt.Sprintf("\nChecking Spreadsheets have the same number of sheets and sheet names"))
+	_ = cmpSheetTitles(gs_old_props, gs_new_props, data)
 	old_content := getSheetContents(gs_old_props, service)
 	new_content := getSheetContents(gs_new_props, service)
 
 	dmp := diffmatchpatch.New()
 	for k, _ := range old_content {
-		info.Printf("\nINFO: Checking %s\n", k)
+		data.Info = append(data.Info , fmt.Sprintf("\nINFO: Checking %s\n", k))
 		diffs := dmp.DiffMain(old_content[k], new_content[k], true)
-		fmt.Println(dmp.DiffPrettyText(diffs))
+		var htmloutput = fmt.Sprintf( "%s", dmp.DiffPrettyHtml(diffs))
+		data.Output = data.Output + template.HTML(htmloutput)
 	}
+	tmpl, err := template.ParseFiles("template.html")
+	tmpl.Execute(w, data)
+	if err != nil {
+		log.Printf("some problem with template %s", err)
+	}
+
 }
 
 func getSheetContents(props MySheet, service *sheets.Service) map[string]string {
@@ -130,13 +168,13 @@ func getSheetContents(props MySheet, service *sheets.Service) map[string]string 
 		this_title := row.Properties.Title
 		resp, err := service.Spreadsheets.Values.Get(props.SpreadsheetID, this_title).ValueRenderOption(valueRenderOption).Do()
 		if err != nil {
-			log.Fatalf("Unable to retrieve data from sheet: %v", err)
+			log.Printf("Unable to retrieve data from sheet: %v", err)
 		}
 		for _, brow := range resp.Values {
 			for _, col := range brow {
 				contents[this_title] += "'" + col.(string) + "',"
 			}
-			contents[this_title] += "\n"
+			contents[this_title] += "\n\n"
 		}
 	}
 	return contents
@@ -144,7 +182,7 @@ func getSheetContents(props MySheet, service *sheets.Service) map[string]string 
 
 //compares 2 spreadsheet's sheet titles and exits if they are not the same
 // and in the same order
-func cmpSheetTitles(old_props MySheet, new_props MySheet) bool {
+func cmpSheetTitles(old_props MySheet, new_props MySheet, data Results) bool {
 	var old_titles, new_titles []string
 
 	for _, row := range old_props.Sheets {
@@ -153,21 +191,27 @@ func cmpSheetTitles(old_props MySheet, new_props MySheet) bool {
 	for i, row := range new_props.Sheets {
 		new_titles = append(new_titles, row.Properties.Title)
 		if row.Properties.Title != old_titles[i] {
-			fmt.Printf("\n%s != %s", row.Properties.Title, old_titles[i])
-			os.Exit(1)
+			data.Info = append(data.Info ,  fmt.Sprintf("\n%s != %s", row.Properties.Title, old_titles[i]))
+			//os.Exit(1)
 			return false
 		}
-		fmt.Printf("\n%s Exists in both Spreadsheets", row.Properties.Title)
+		data.Info = append(data.Info ,  fmt.Sprintf("\n%s Exists in both Spreadsheets", row.Properties.Title))
 	}
 	if len(old_titles) != len(new_titles) {
-		fmt.Println("\nSpreadsheets have different number of Sheets")
-		fmt.Printf("\nThe old Spreadsheet has %d and the new %d", len(old_titles), len(new_titles))
-		os.Exit(1)
+		data.Info = append(data.Info , fmt.Sprintf("\nSpreadsheets have different number of Sheets"))
+		data.Info = append(data.Info ,  fmt.Sprintf("\nThe old Spreadsheet has %d and the new %d", len(old_titles), len(new_titles)))
+		//os.Exit(1)
 		return false
 	}
 
-	fmt.Printf("\nINFO:%s", "Sheets have the same titles")
+	data.Info = append(data.Info , fmt.Sprintf("\nINFO:%s", "Sheets have the same titles"))
 	return true
+}
+
+
+func errorToScreen(error string, w http.ResponseWriter) {
+	log.Printf("%s", error)
+	fmt.Fprintf(w, "%s", error)
 }
 
 // From Google api quickstart https://github.com/gsuitedevs/go-samples/blob/master/sheets/quickstart/quickstart.go
@@ -190,12 +234,12 @@ func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
 
 	var authCode string
 	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code: %v", err)
+		log.Printf("Unable to read authorization code: %v", err)
 	}
 
 	tok, err := config.Exchange(oauth2.NoContext, authCode)
 	if err != nil {
-		log.Fatalf("Unable to retrieve token from web: %v", err)
+		log.Printf("Unable to retrieve token from web: %v", err)
 	}
 	return tok
 }
@@ -218,7 +262,7 @@ func saveToken(path string, token *oauth2.Token) {
 	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
 	defer f.Close()
 	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
+		log.Printf("Unable to cache oauth token: %v", err)
 	}
 	json.NewEncoder(f).Encode(token)
 }
@@ -226,20 +270,20 @@ func saveToken(path string, token *oauth2.Token) {
 func qsmain()(*sheets.Service) {
 	b, err := ioutil.ReadFile("credentials.json")
 	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
+		log.Printf("Unable to read client secret file: %v", err)
 	}
 
 	// If modifying these scopes, delete your previously saved token.json.
 	config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets.readonly")
 	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
+		log.Printf("Unable to parse client secret file to config: %v", err)
 	}
 	client := getClient(config)
 
 	srv, err := sheets.New(client)
 
 	if err != nil {
-		log.Fatalf("Unable to retrieve Sheets client: %v", err)
+		log.Printf("Unable to retrieve Sheets client: %v", err)
 	}
 	return srv
 }
